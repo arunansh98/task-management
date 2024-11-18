eed3f61abece046597af89887a661576cbff825a
Arunansh Srivastava <arunansh98@gmail.com>
2024-11-18 23:48:28 +0530

    Removed unused classes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,15 +4,12 @@ function Modal({ ...props }) {
   const { setShowEditModal } = props;
   const modalOverlay = "fixed inset-0 bg-gray-300 opacity-80";
   const modalContent =
-    "fixed text-center border-[1px] border-solid border-[grey] inset-[35rem] top-[8rem] bg-white opacity-100";
+    "fixed text-center border-[1px] border-solid border-[grey] inset-[35rem] top-[8rem] bg-white";
   return ReactDOM.createPortal(
-    <div className={modalOverlay} style={{ zIndex: 1000 }}>
-      <div className={modalContent} style={{ zIndex: 1001 }}>
-        <div
-          className="flex justify-end mr-4 mt-1 cursor-pointer"
-          onClick={() => setShowEditModal(false)}
-        >
-          X
+    <div className={modalOverlay}>
+      <div className={modalContent}>
+        <div className="flex justify-end mr-4 mt-1">
+          <button onClick={() => setShowEditModal(false)}>X</button>
         </div>
       </div>
     </div>,
